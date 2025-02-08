e1d55ed92e3ecd0d377c08075617297c235b20c4
Priti Modanwal <pritivns612@gmail.com>
2025-02-08 23:46:55 +0530

    homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,98 +3,74 @@ import './Homepage.css';
 
 function Homepage() {
   return (
-    <div className="classroom-list1">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>JavaScript</td>
-            <td>Used for web development, creating interactive UI, and server-side scripting (Node.js).</td>
-          </tr>
-          <tr>
-            <td>Python</td>
-            <td>Popular for data science, machine learning, web development, and automation.</td>
-          </tr>
-          <tr>
-            <td>Java</td>
-            <td>Used in enterprise applications, Android development, and backend services.</td>
-          </tr>
-          <tr>
-            <td>C</td>
-            <td>A foundational language for system programming, embedded systems, and OS development.</td>
-          </tr>
-          <tr>
-            <td>C++</td>
-            <td>Used in game development, high-performance applications, and competitive programming.</td>
-          </tr>
-          <tr>
-            <td>C#</td>
-            <td>Mainly used for Windows applications, game development (Unity), and enterprise solutions.</td>
-          </tr>
-          <tr>
-            <td>PHP</td>
-            <td>A server-side language for web development, especially for backend scripting (WordPress, Laravel).</td>
-          </tr>
-          <tr>
-            <td>Swift</td>
-            <td>Used for iOS and macOS app development.</td>
-          </tr>
-          <tr>
-            <td>Go (Golang)</td>
-            <td>Efficient for cloud computing, microservices, and backend development.</td>
-          </tr>
-          <tr>
-            <td>Kotlin</td>
-            <td>Preferred for Android development and modern backend applications.</td>
-          </tr>
-          <tr>
-            <td>R</td>
-            <td>Specialized for statistical computing, data visualization, and machine learning.</td>
-          </tr>
-          <tr>
-            <td>TypeScript</td>
-            <td>A superset of JavaScript, enhancing web application scalability and maintainability.</td>
-          </tr>
-          <tr>
-            <td>Ruby</td>
-            <td>Used for web development, particularly with the Ruby on Rails framework.</td>
-          </tr>
-          <tr>
-            <td>Rust</td>
-            <td>Highly performant and memory-safe, used in systems programming and web assembly.</td>
-          </tr>
-          <tr>
-            <td>SQL</td>
-            <td>Used for managing and querying relational databases.</td>
-          </tr>
-          <tr>
-            <td>Lua</td>
-            <td>A lightweight, high-performance scripting language used in game development, embedded systems, and networking applications.</td>
-          </tr>
-          <tr>
-            <td>Perl</td>
-            <td>Known for text processing, it is often used in web development, system administration, and network programming.</td>
-          </tr>
-          <tr>
-            <td>Shell Scripting</td>
-            <td>Used for automating tasks in Unix/Linux environments, such as file manipulation, program execution, and text processing.</td>
-          </tr>
-          <tr>
-            <td>Elixir</td>
-            <td>Used for building scalable and maintainable applications, especially in real-time systems like messaging platforms and IoT.</td>
-          </tr>
-          <tr>
-            <td>Haskell</td>
-            <td>A functional programming language used in academic research, compilers, and complex algorithms, known for its strong type system.</td>
-          </tr>
-
-        </tbody>
-      </table>
+    <div className="container">
+      <div className='card_container'>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/javascript-1.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/JavaScriptBook/' target='blank'>Download Book</a>
+        </div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/css-3.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/CSSBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/html-1.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/HTML5Book/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/angular-icon-1.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/AngularJSBook/' target='blank'>Download Book</a>
+          </div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/python-5.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/PythonBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/spring-3.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/SpringFrameworkBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/react-2.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/ReactJSBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/tailwindcss.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/CSSBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/react-native-1.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/ReactNativeBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/react-native-firebase-1.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/ReactNativeBook/' target='blank'>Download Book</a></div>
+          <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/jquery-4.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/jQueryBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/git-icon.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/GitBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/typescript.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/TypeScriptBook2/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/ruby.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/RubyBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/mongodb-icon-2.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/MongoDBBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/kotlin-1.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/KotlinBook/' target='blank'>Download Book</a></div>
+          <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/laravel-2.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/PHPBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/mysql-logo-pure.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/MySQLBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/java-4.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/JavaBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/c.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/CPlusPlusBook/' target='blank'>Download Book</a></div>
+        <div className='card'>
+          <img className='image' src='https://cdn.worldvectorlogo.com/logos/c-1.svg' alt='not image' />
+          <a className='download' href='https://books.goalkicker.com/CBook/' target='blank'>Download Book</a></div>
+      </div>
     </div>
   );
 }
